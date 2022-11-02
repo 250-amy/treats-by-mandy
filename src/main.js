c36104fdf7cf0298fc38a58d91f0fbf25b63cca5
250-amy <amy.tencer@gmail.com>
2022-11-02 09:26:04 -0700

    install vue icon pack

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { FaFacebookF, BiInstagram, BiGoogle } from "oh-vue-icons/icons"
 
+addIcons(FaFacebookF, BiInstagram, BiGoogle)
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component("v-icon", OhVueIcon);
+app.mount("#app");
